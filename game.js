3ec84f7b25fa2900b0f6ea4389f65dc472536e92
samlarson94 <samlarson94@gmail.com>
2022-07-29 14:12:39 -0500

    Setting up files and initializing Kaboom.js

---
+++ game.js
@@ -0,0 +1,16 @@
+const { StarTwoTone } = require("@material-ui/icons")
+
+// Initialize Kaboom
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 1,
+    debug: true,
+})
+
+screen("game", () => {
+
+})
+
+// Start Game
+start("game")
