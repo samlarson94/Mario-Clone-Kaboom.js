c4d1981e54912572683ca310422f722d8b61f840
samlarson94 <samlarson94@gmail.com>
2022-08-03 09:41:44 -0500

    Adding logic under pipe player.collides function to loop over the two levels created.

--- game.js
+++ game.js
@@ -223,7 +223,7 @@ scene("game", ({ level, score }) => {
     player.collides('pipe', () => {
         keyDown('down', () => {
             go('game', {
-                level: level + 1,
+                level: (level + 1) % maps.length,
                 score: scoreLabel.value,
             })
         })
